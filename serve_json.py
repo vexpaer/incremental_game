5a886497d00b20d076b4e31ec5213292fdc3e826
vexpaer <57309907+vexpaer@users.noreply.github.com>
2024-09-22 19:52:23 +0800

    Create serve_json.py

---
+++ serve_json.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # 启用CORS
+
+@app.route('/get-json-machine')
+def get_json1():
+    return send_from_directory('.', 'machine.json', as_attachment=True)
+
+@app.route('/get-json-resource')
+def get_json2():
+    return send_from_directory('.', 'resource.json', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
